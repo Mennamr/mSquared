873ff7949ca7818225f2b58432933ac857b9aed6
mennamr <mennamrahmed@gmail.com>
2025-11-09 14:56:36 +0200

    added environments

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -2,20 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { AuthUser, LoginUser } from '../../interfaces/auth-user';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   _httpClient=inject(HttpClient);
+  env=environment.baseURL
 
   constructor() { }
 
   registerUser(userInfo:AuthUser):Observable<any> {
-    return this._httpClient.post(`https://ecommerce.routemisr.com/api/v1/auth/signup`, userInfo)
+    return this._httpClient.post(`${this.env}/auth/signup`, userInfo)
   }
 
   loginUser(userInfo:LoginUser):Observable<any> {
-    return this._httpClient.post(`https://ecommerce.routemisr.com/api/v1/auth/signin`, userInfo)
+    return this._httpClient.post(`${this.env}/auth/signin`, userInfo)
   }
 }
