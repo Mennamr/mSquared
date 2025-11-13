1b74d1463035b7ef309d53477bd5a8cefa0d577b
mennah125 <mennazqqq@gmail.com>
2025-11-13 12:04:49 +0200

    checkot

--- src/app/features/pages/products/products.component.ts
+++ src/app/features/pages/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RecentProductsComponent } from '../home/components/recent-products/recent-products.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RecentProductsComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
